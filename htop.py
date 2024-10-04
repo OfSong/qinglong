2364c8657d585b7544c14cb0b56c90377def8e38
ofSong <103166550+OfSong@users.noreply.github.com>
2024-10-04 15:42:53 +0800

    Update htop.py

--- htop.py
+++ htop.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # -- coding: utf-8 --
 # -------------------------------
-# cron "0 */3 * * *" script-path=xxx.py,tag=匹配cron用
+# cron "0 */1 * * *" script-path=xxx.py,tag=匹配cron用
 # const $ = new Env('监控系统状态')
 
 import psutil
